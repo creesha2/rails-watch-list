1e01c80c90cfe87616f92e6976a2e20b91bf09e1
Pelin Chatzi Souleiman <pelin2495@hotmail.com>
2025-05-29 16:33:11 +0200

    Routing, Controller, Views for Lists

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  root to: "lists#index"
 end
